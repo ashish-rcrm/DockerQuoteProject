6d200154c56e2151cd9038b4114abc3844f5118b
ashish-rcrm <ashishbhatia@recruitcrm.io>
2022-06-22 16:33:11 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">
+          Info: click on a quote to delete it
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import QuoteGrid from "./components/QuoteGrid.vue"
+  import NewQuote from "./components/NewQuote.vue"
+  import Header from "./components/AppHeader.vue"
+
+  export default {
+    data(){
+      return {
+        quotes : [
+          "This is a quote"
+        ],
+        maxQuotes : 10
+      }
+    },
+    methods : {
+      newQuote(quote){
+        if(this.quotes.length >= this.maxQuotes){
+          return alert("Please delete a Quote first")
+        }
+        this.quotes.push(quote);
+      },
+      deleteQuote(index){
+        console.log("working");
+        this.quotes.splice(index,1);
+      }
+    },
+    components: {
+      appQuoteGrid : QuoteGrid,
+      appNewQuote : NewQuote,
+      appHeader : Header
+    }
+}
+</script>
+
+<style>
+</style>
